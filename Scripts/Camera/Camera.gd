958bf31d8933e03034dffb144e26ec67999f608c
Tobazsbasement <friendlypieceofshit@gmail.com>
2023-08-31 12:27:30 +0200

    Added Camera Zoom
    -Made the camera zoom in when pinching on touch screen

--- Scripts/Camera/Camera.gd
+++ Scripts/Camera/Camera.gd
@@ -6,6 +6,9 @@ extends Camera2D
 @export var can_zoom:bool
 
 var touch_points:Dictionary = {}
+var start_distance
+var start_zoom
+
 func _input(event):
 	if event is InputEventScreenTouch:
 		handle_touch(event)
@@ -17,6 +20,14 @@ func handle_touch(event: InputEventScreenTouch):
 		touch_points[event.index] = event.position
 	else:
 		touch_points.erase(event.index)
+	
+	if touch_points.size() == 2:
+		var touch_point_positions = touch_points.values()
+		start_distance = touch_point_positions[0].distance_to(touch_point_positions[1])
+		
+		start_zoom = zoom
+	elif touch_points.size() < 2:
+		start_distance = 0
 
 func handle_drag(event: InputEventScreenDrag):
 	touch_points[event.index] = event.position
@@ -24,3 +35,22 @@ func handle_drag(event: InputEventScreenDrag):
 	if touch_points.size() == 1:
 		if can_pan:
 			offset -= event.relative * pan_speed
+	elif touch_points.size() == 2:
+		var touch_point_positions = touch_points.values()
+		var current_dist = touch_point_positions[0].distance_to(touch_point_positions[1])
+		
+		var zoom_factor = start_distance/current_dist
+		if can_zoom:
+			zoom = start_zoom / zoom_factor
+		limit_zoom(zoom)
+
+func limit_zoom(new_zoom):
+	if new_zoom.y > 10:
+		zoom.y = 10
+	if new_zoom.x > 10:
+		zoom.x = 10
+	if new_zoom.y < 0.1:
+		zoom.y = 0.1
+	if new_zoom.x < 0.1:
+		zoom.x = 0.1
+	print(zoom)
